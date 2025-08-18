4bb99c4f620f665f6b69b454d4325e622695faba
lucasgeruntho <136639689+lucasgeruntho@users.noreply.github.com>
2025-08-18 19:48:00 -0300

    salve2

---
+++ mobile/fundo.js
@@ -0,0 +1,20 @@
+VANTA.HALO({
+  el: ".background-topo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 590.00,
+  minWidth: 200.00,
+  backgroundColor: 0x22307c,
+  amplitudeFactor: 1.90,
+  xOffset: 0.01,
+  yOffset: -0.08,
+  size: 2.90
+})
+
+window.addEventListener('scroll', function() {
+  const circulo = document.querySelector('.circulo span');
+  const rotation = window.scrollY % 360;
+  circulo.style.transform = `rotate(${rotation}deg)`;
+});
+
---
+++ mobile/scriptmobile.js
@@ -0,0 +1,66 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("social-toggle");
+    const popup = document.getElementById("social-popup");
+
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      const rect = toggle.getBoundingClientRect();
+
+      // Posiciona abaixo do botão
+      popup.style.top = `${rect.bottom + window.scrollY + 25}px`;
+      popup.style.left = `${rect.left + window.scrollX + -25}px`;
+
+      popup.style.display = popup.style.display === "flex" ? "none" : "flex";
+    });
+
+    document.addEventListener("click", function (e) {
+      if (!popup.contains(e.target) && !toggle.contains(e.target)) {
+        popup.style.display = "none";
+      }
+    });
+  });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.getElementById("mobile-menu-btn");
+  const menu = document.getElementById("mobile-menu");
+  const submenu = document.querySelector(".submenu");
+
+  menuBtn.addEventListener("click", (e) => {
+    e.stopPropagation(); 
+    const isActive = menuBtn.classList.toggle("active"); 
+    menu.style.display = isActive ? "flex" : "none";
+
+   
+    if (isActive) {
+      submenu.classList.add("active");
+    } else {
+      submenu.classList.remove("active");
+    }
+  });
+
+  document.addEventListener("click", () => {
+    if (menuBtn.classList.contains("active")) {
+      menuBtn.classList.remove("active");
+      menu.style.display = "none";
+      submenu.classList.remove("active");
+    }
+  });
+
+
+  submenu.addEventListener("click", () => {
+    submenu.classList.toggle("active");
+  });
+});
+
+
+const btn = document.querySelector('.circle-btn');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('active');
+});
+
+
+
+
+

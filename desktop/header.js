3192db5d19dc13a317e8c1079862530d17b7131c
lucasgeruntho <136639689+lucasgeruntho@users.noreply.github.com>
2025-08-21 22:55:00 -0300

    salve7

---
+++ desktop/header.js
@@ -0,0 +1,25 @@
+
+// pop-up do desktop
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("#social-toggle");
+    const popup = document.getElementById("#social-popup");
+
+    toggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      const rect = toggle.getBoundingClientRect();
+
+      // Posiciona abaixo do botão
+      popup.style.top = `${rect.bottom + window.scrollY + 25}px`;
+      popup.style.left = `${rect.left + window.scrollX + -25}px`;
+
+      popup.style.display = popup.style.display === "flex" ? "none" : "flex";
+    });
+
+    document.addEventListener("click", function (e) {
+      if (!popup.contains(e.target) && !toggle.contains(e.target)) {
+        popup.style.display = "none";
+      }
+    });
+  });

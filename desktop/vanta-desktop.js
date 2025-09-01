f7c748a3c6afca05c04ed81d72837a0ef85ab915
lucasgeruntho <136639689+lucasgeruntho@users.noreply.github.com>
2025-09-01 17:17:53 -0300

    salve17

---
+++ desktop/vanta-desktop.js
@@ -0,0 +1,15 @@
+VANTA.DOTS({
+  el: "#vanta-section",
+   mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffffff,
+  color2: 0xffffff,
+  backgroundColor: 0x1045bd,
+  size: 1.60,
+  spacing: 44.00
+})
